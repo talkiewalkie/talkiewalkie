51cc6eec62bfc06ba24f5ffd985f7650a5d596b9
theo <theo@matussie.re>
2021-04-16 21:24:58 +0200

    no postgis

--- server/migrations/003_postgis.down.sql
+++ server/migrations/003_postgis.down.sql
@@ -2,4 +2,5 @@ ALTER TABLE walk
     DROP COLUMN start_point,
     DROP COLUMN end_point;
 
-DROP EXTENSION postgis;
+DROP EXTENSION IF EXISTS cube;
+DROP EXTENSION IF EXISTS earthdistance;
--- server/migrations/003_postgis.up.sql
+++ server/migrations/003_postgis.up.sql
@@ -1,5 +1,10 @@
-CREATE EXTENSION postgis;
+-- https://hashrocket.com/blog/posts/juxtaposing-earthdistance-and-postgis
+-- -> no postgis is simpler and we don't need to be extra accurate
+-- -> "cube" distance is best:
+--    SELECT *, earth_distance(ll_to_earth(point[0], point[1]),  ll_to_earth(32, 2)) as d from table;
+CREATE EXTENSION IF NOT EXISTS cube;
+CREATE EXTENSION IF NOT EXISTS earthdistance;
 
 ALTER TABLE walk
-    ADD COLUMN start_point geography(point),
-    ADD COLUMN end_point   geography(point);
+    ADD COLUMN start_point POINT NOT NULL,
+    ADD COLUMN end_point   POINT NOT NULL;
