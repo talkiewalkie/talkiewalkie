23047f77ee639042ec8cbbf6c805236a00f7d40c
Théo Matussière <theo@matussie.re>
2021-08-03 18:44:12 +0200

    management modal (blurred background)
    unused draggable component
    editor content not leveraged yet

--- rn/screens/Editor.tsx
+++ rn/screens/Editor.tsx
@@ -1,19 +1,46 @@
-import React, { useEffect, useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { Animated, Modal, Text, TouchableOpacity, View } from "react-native";
 import { StackScreenProps } from "@react-navigation/stack";
 import { MainNavigatorParamList } from "../navigation/MainNavigator";
 import { Audio } from "expo-av";
+import { BlurView } from "expo-blur";
+import styled from "styled-components/native";
+import { blankEditorContext, EditorContext } from "../hooks/useEditor";
 
 export const Editor = ({
   route,
   navigation,
+}: StackScreenProps<MainNavigatorParamList, "EDITOR">) => (
+  <EditorContext.Provider value={blankEditorContext}>
+    <WrappedEditor navigation={navigation} route={route} />
+  </EditorContext.Provider>
+);
+
+export const WrappedEditor = ({
+  route,
+  navigation,
 }: StackScreenProps<MainNavigatorParamList, "EDITOR">) => {
+  const uuid = route.params.uuid;
+
+  const [showMgmtModal, setShowMgmtModal] = useState(false);
+
   const [recording, setRecording] = useState<Audio.Recording>();
   const [sound, setSound] = useState<Audio.Sound>();
   const [duration, setDuration] = useState<number>();
   const [isPlaying, setIsPlaying] = useState(false);
 
-  const uuid = route.params.uuid;
+  const recHeight = useRef(new Animated.Value(0)).current;
+  recHeight.addListener((v) => setDuration(v.value));
+
+  useEffect(() => {
+    Animated.timing(recHeight, {
+      toValue: 10000,
+      duration: 30,
+      // delay: 0,
+      // easing: () => 4,
+      useNativeDriver: true,
+    }).start();
+  }, [recHeight]);
 
   useEffect(() => {
     sound
@@ -56,7 +83,27 @@ export const Editor = ({
           backgroundColor: "rgb(202,225,227)",
         }}
       />
-
+      {recording && (
+        <View
+          style={{
+            position: "absolute",
+            left: "50%",
+            bottom: 100,
+            transform: [{ translateX: -30 }],
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            width: 60,
+            backgroundColor: isPlaying
+              ? "rgb(255, 255, 255)"
+              : "rgb(100, 100, 100)",
+            // height: 100,
+            height: (duration ?? 2000) / 100,
+          }}
+        />
+      )}
+      {/*// TODO: make this draggable */}
+      {/*//  https://www.youtube.com/watch?v=tHWGKdpj1rs*/}
+      {/*    https://snack.expo.io/@yoobidev/draggable-component*/}
       {sound && duration && (
         <TouchableOpacity
           style={{
@@ -69,8 +116,10 @@ export const Editor = ({
             width: 60,
             backgroundColor: isPlaying
               ? "rgb(255, 255, 255)"
-              : "rgb(100, 100, 100)",
-            height: duration / 100,
+              : "rgb(173,127,127)",
+            borderWidth: 2,
+            borderColor: "rgba(0,0,0,0.24)",
+            height: duration / 50,
           }}
           onPress={async () => {
             if (isPlaying) {
@@ -147,6 +196,126 @@ export const Editor = ({
       >
         <Text>{recording ? "p" : "m"}</Text>
       </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          position: "absolute",
+          right: 30,
+          top: 50,
+
+          borderRadius: 40,
+          width: 40,
+          height: 40,
+
+          backgroundColor: "rgb(255,255,255)",
+
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        onPress={() => setShowMgmtModal(true)}
+      >
+        <Text>...</Text>
+      </TouchableOpacity>
+      <View
+        style={{
+          marginVertical: 100,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Modal
+          visible={showMgmtModal}
+          animationType="fade"
+          style={{
+            margin: 0,
+          }}
+          onRequestClose={() => setShowMgmtModal(false)}
+          onStartShouldSetResponder={() => {
+            setShowMgmtModal(false);
+            console.log("hey");
+            return true;
+          }}
+          transparent
+        >
+          <BlurView
+            tint="default"
+            intensity={80}
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <View
+              style={{
+                backgroundColor: "white",
+                borderRadius: 10,
+                padding: 10,
+                width: "90%",
+                height: "70%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+
+                shadowColor: "rgb(205,147,147)",
+                shadowOffset: {
+                  width: 0,
+                  height: 4,
+                },
+                shadowOpacity: 0.28,
+                shadowRadius: 4.95,
+                elevation: 2,
+              }}
+            >
+              <CloseModalButton onPress={() => setShowMgmtModal(false)} />
+              <ModalOption>
+                <Text>Save draft</Text>
+              </ModalOption>
+              <ModalOption>
+                <Text>Publish</Text>
+              </ModalOption>
+            </View>
+          </BlurView>
+        </Modal>
+      </View>
     </View>
   );
 };
+
+const ModalOption = styled.TouchableOpacity`
+  background-color: rgb(255, 255, 255);
+  padding: 10px;
+  color: rgb(255, 0, 0);
+`;
+
+const CloseModalButton = ({ onPress }: { onPress: () => void }) => (
+  <TouchableOpacity
+    style={{
+      position: "absolute",
+      top: 16,
+      right: 16,
+      borderRadius: 30,
+      height: 30,
+      width: 30,
+
+      backgroundColor: "rgb(177,177,177)",
+
+      shadowColor: "rgb(205,147,147)",
+      shadowOffset: {
+        width: 0,
+        height: 4,
+      },
+      shadowOpacity: 0.28,
+      shadowRadius: 4.95,
+      elevation: 2,
+
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    }}
+    onPress={onPress}
+  >
+    <Text style={{ color: "white" }}>x</Text>
+  </TouchableOpacity>
+);
