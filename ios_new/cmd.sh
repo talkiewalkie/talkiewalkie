59ae5351b3203aabc3fa98422057de81cbe3e4b6
Théo Matussière <theo@matussie.re>
2021-10-14 18:02:37 +0200

    Revert "move protos to server"
    This reverts commit dbeb1943585f104b7a4a4ffc798664c5420dabf5.

--- ios_new/cmd.sh
+++ ios_new/cmd.sh
@@ -18,14 +18,14 @@ ios_dev_install() {
 
 codegen() {
   protoc -I=protos \
-      --proto_path=../server/protos \
+      --proto_path=../protos \
       --plugin="${HOME}/.local/bin/protoc-gen-swift" \
       --swift_opt=Visibility=Public \
       --swift_out=TalkieWalkie/Clients \
       --plugin="${HOME}/.local/bin/protoc-gen-grpc-swift" \
       --grpc-swift_opt=Visibility=Public \
       --grpc-swift_out=TalkieWalkie/Clients \
-      ../server/protos/app.proto
+      ../protos/app.proto
 }
 
 "$@"
